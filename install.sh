8ca565032d2d57dfd53ed63320b8981903d4e8a8
A learning person <ansonwan1988@gmail.com>
2020-02-13 18:00:54 +0800

    更新hadowsocks-all.sh安装脚本

--- install.sh
+++ install.sh
@@ -38,29 +38,29 @@ cur_dir=$( pwd )
 software=(Shadowsocks-Python ShadowsocksR Shadowsocks-Go Shadowsocks-libev)
 
 libsodium_file="libsodium-1.0.17"
-libsodium_url="https://github.com/man2018/sss/blob/master/4in1/libsodium-1.0.17.tar.gz"
+libsodium_url="https://github.com/man2018/sss/raw/master/libsodium/libsodium-1.0.17.tar.gz"
 
 mbedtls_file="mbedtls-2.16.0"
-mbedtls_url="https://github.com/man2018/sss/blob/master/4in1/mbedtls-2.16.0-gpl.tgz"
+mbedtls_url="https://github.com/man2018/sss/raw/master/mbedtls/mbedtls-2.16.0-gpl.tgz"
 
 shadowsocks_python_file="shadowsocks-master"
-shadowsocks_python_url="https://github.com/man2018/sss/blob/master/4in1/master.zip"
+shadowsocks_python_url="https://github.com/man2018/sss/raw/master/shadowsocks_python/shadowsocks-master.zip"
 shadowsocks_python_init="/etc/init.d/shadowsocks-python"
 shadowsocks_python_config="/etc/shadowsocks-python/config.json"
 shadowsocks_python_centos="https://raw.githubusercontent.com/man2018/sss/master/shadowsocks"
 shadowsocks_python_debian="https://raw.githubusercontent.com/man2018/sss/master/shadowsocks-debian"
 
 shadowsocks_r_file="shadowsocksr-3.2.2"
-shadowsocks_r_url="https://github.com/man2018/sss/blob/master/4in1/shadowsocksr-3.2.2.tar.gz"
+shadowsocks_r_url="https://github.com/man2018/sss/raw/master/shadowsocks_r/shadowsocksr-3.2.2.tar.gz"
 shadowsocks_r_init="/etc/init.d/shadowsocks-r"
 shadowsocks_r_config="/etc/shadowsocks-r/config.json"
 shadowsocks_r_centos="https://raw.githubusercontent.com/man2018/sss/master/shadowsocksR"
 shadowsocks_r_debian="https://raw.githubusercontent.com/man2018/sss/master/shadowsocksR-debian"
 
 shadowsocks_go_file_64="shadowsocks-server-linux64-1.2.2"
-shadowsocks_go_url_64="https://github.com/man2018/sss/blob/master/4in1/shadowsocks-server-linux64-1.2.2.gz"
+shadowsocks_go_url_64="https://github.com/man2018/sss/raw/master/shadowsocks_go/shadowsocks-server-linux64-1.2.2.gz"
 shadowsocks_go_file_32="shadowsocks-server-linux32-1.2.2"
-shadowsocks_go_url_32="https://github.com/man2018/sss/blob/master/4in1/shadowsocks-server-linux32-1.2.2.gz"
+shadowsocks_go_url_32="https://github.com/man2018/sss/raw/master/shadowsocks_go/shadowsocks-server-linux32-1.2.2.gz"
 shadowsocks_go_init="/etc/init.d/shadowsocks-go"
 shadowsocks_go_config="/etc/shadowsocks-go/config.json"
 shadowsocks_go_centos="https://raw.githubusercontent.com/man2018/sss/master/shadowsocks-go"
